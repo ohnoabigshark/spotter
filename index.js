96e1cf655812eadffc98416c0330be465fe7f098
ohnoabigshark <ohnoabigshark@gmail.com>
2021-01-03 14:59:35 -0500

    Starting to add database schema in database_setup.txt. Will add more to this as schema is designed and put into psql.

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
+const pg = require('pg');
 
 
 app.use(express.static("public"));
 
 app.get("/", function(request, response) {
 	response.send("Server is up.");	
+	let sqlQuery = "select * from noteentries where noteid = "+NOTE.currentNoteId+" order by entryid asc";
+	console.log(sqlQuery);
+	pg.connect(process.env.DATABASE_URL, function ( err, client, done ) {
+		client.query(sqlQuery, function(err, result) {
+			//console.log(result.rows);
+			NOTE.entries = result.rows;
+			VASH.changeState(STATE.Note);
+			io.emit('openNote',NOTE.getData());
+			done();
+		}); 
+	});
 });
 
 app.listen(process.env.PORT || 3000, () => console.log("Spotter: index.js started."));
