00ba9c1b7d4a8e29d43b9ede19741f4554a41b5f
ohnoabigshark <ohnoabigshark@gmail.com>
2021-10-23 01:36:14 -0400

    Checking in master so I can branch off to convert to Typescript.

--- test/CraigslistScraper.test.js
+++ test/CraigslistScraper.test.js
@@ -13,3 +13,5 @@ test ('Create scraper and scrape.', async () => {
 	//THEN
 	expect(result).not.toBe(null); //TODO: Should also expect result to have a size that is equal to the number of listings in a web page
 });
+
+//TODO: Create tests to see if specific data is returned by the Listing objects
